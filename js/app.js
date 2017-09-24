87721ea42117992482b6b8ba2d6542474b86777a
Sakib Shamin <Sakib.Shamin@gmail.com>
2017-09-25 02:00:53 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 	var link = $('.modal-window iframe').attr('src');
+	if($(window).width() < 768)
+	{
+		$('.modal-window iframe').attr('width', '250');
+		$('.modal-window iframe').attr('height', '150');
+	}
+	
+	else if($(window).width() >= 768 && $(window).width() < 992)
+	{
+		$('.modal-window iframe').attr('width', '400');
+		$('.modal-window iframe').attr('height', '250');
+	}
+
+	else
+	{
+		$('.modal-window iframe').attr('width', '560');
+		$('.modal-window iframe').attr('height', '315');
+	}
 	$('#playVideo').click(function() {
 		link += '?autoplay=1';
 		$('.modal-window iframe').attr('src', link);
